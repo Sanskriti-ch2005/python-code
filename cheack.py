95d21f959428ab95ceb8c4e0572c18f28a3fdfb6
sanskritichouhan <sanskritichouhan07@gmail.com>
2025-06-06 10:26:52 +0530

    first commit

---
+++ cheack.py
@@ -0,0 +1,7 @@
+num = int(input("enter the value:"))
+if num >= 0:
+    print("the number is positive:", num)
+elif num <= 0:
+    print("the number is negative:",num)
+else:
+    print("the number is zero:", num)
