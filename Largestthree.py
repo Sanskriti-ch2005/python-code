7358dfa86bfe33f0c2ade09e52c8a54f1466b023
sanskritichouhan <sanskritichouhan07@gmail.com>
2025-06-09 17:09:07 +0530

    first commit

---
+++ Largestthree.py
@@ -0,0 +1,9 @@
+a = int(input("enter the number"))
+b = int(input("enter the number"))
+c = int(input("enter the number"))
+if a >= b and a >= c :
+    print("a is largest number", a)
+elif b >= a and b >= c :
+    print("b is largest number", b)
+else:
+    print("c is largert number", c)
