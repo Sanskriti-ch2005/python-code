42eb77b76fe90b767cf35d511b8a6881b87967db
sanskritichouhan <sanskritichouhan07@gmail.com>
2025-06-10 17:14:24 +0530

    married.py

---
+++ married.py
@@ -0,0 +1,9 @@
+#gender = str(input("enter the gender:"))
+#age = int(input("enter the age:"))
+status1 = str(input("enter the status(married/ unmarried)"))
+if  status1 == "unmarried" :
+    print("you are selected for this role", status1)
+elif  status1 == "unmarried":
+    print("you are selected for this role:", status1)
+else:
+    print("you are not matched with carteria")
