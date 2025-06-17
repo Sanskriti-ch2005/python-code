9bc6dbda1be0dbcb2f1eb9930e11554247a29b3e
sanskritichouhan <sanskritichouhan07@gmail.com>
2025-06-17 17:24:21 +0530

    alphabet

--- alphabet.py
+++ alphabet.py
@@ -1,5 +1,5 @@
-row= int(input("enter the row"))
-for i in range(1,row+1):
-    for j in range(65, 65+i):
-        print(chr(j), end='')
-    print()
+i = ord('a')
+while i <= ord('z'):
+    ch = chr(i)
+    print(ch)
+    i += 1
